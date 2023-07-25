6dffeaa3de0938924a706d6a081abcf3cf5a91d4
Daniel Ortega <dortega@suitsupply.com>
2023-07-25 11:25:55 +0200

    add netlify middleware

---
+++ middleware.ts
@@ -0,0 +1,24 @@
+import { NextRequest } from "next/server";
+import { MiddlewareRequest } from "@netlify/next";
+import { renderToString } from "@suits/ss-design-system/hydrate";
+
+const PUBLIC_FILE = /\.(.*)$/;
+
+export async function middleware(nextRequest: NextRequest) {
+  if (
+    nextRequest.nextUrl.pathname.startsWith("/_next") ||
+    nextRequest.nextUrl.pathname.includes("/api/") ||
+    PUBLIC_FILE.test(nextRequest.nextUrl.pathname)
+  ) {
+    return;
+  }
+
+  const middlewareRequest = new MiddlewareRequest(nextRequest as any);
+  const response = await middlewareRequest.next();
+  const baseHtml = await response.text();
+  const hydratedHtml = (await renderToString(baseHtml)).html;
+
+  response.text = () => Promise.resolve(hydratedHtml);
+
+  return response;
+}
